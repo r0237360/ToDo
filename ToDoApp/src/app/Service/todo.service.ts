db9c389524a317d17f56e0752414a925bc525d84
Stijn <r0237360@student.thomasmore.be>
2020-06-10 10:18:07 +0200

    checkbox checked methoden toegevoegd

--- ToDoApp/src/app/Service/todo.service.ts
+++ ToDoApp/src/app/Service/todo.service.ts
@@ -22,50 +22,53 @@ export class TodoService {
 
  // Creeer een array met data
   todos: ToDo[] = [
-    new ToDo(1, 'Winkelen', 'Ga naar de winkel',true),
-    new ToDo(2, 'Tuinieren', 'Werk in de tuin',true),
-    new ToDo(3, 'Auto wassen', 'Was de auto',false),
-    new ToDo(4, 'Afval', 'Zet afval buiten',true),
-    new ToDo(5, 'Betaal rekeningen', 'Betaal openstaande rekeningen',false),
-    new ToDo(6, 'Belastingen', 'Vul je belastingaangifte in',false),
-  
+    new ToDo(1, 'Afval', 'Zet afval buiten',true),
+    new ToDo(2, 'Betaal rekeningen', 'Betaal openstaande rekeningen',false),
+    new ToDo(3, 'Tuinieren', 'Werk in de tuin',true),
+    new ToDo(4, 'Winkelen', 'Ga naar de winkel',true),
+    new ToDo(5, 'Belastingen', 'Vul je belastingaangifte in',false),
+    new ToDo(6, 'Auto wassen', 'Was de auto',false),
   ]
 
-  // returns een array met alle items
+  // returns een array met alle ToDo items
   getAllTodos(): ToDo[]{
     return this.todos;
   }  
-  // returns een array met alleen de items waar done = true 
+  // returns een array van ToDo's met alleen de items waar done = true 
   getOnlyTodos(): ToDo[]{
     return this.todos.filter(t => t.done === false);
   }
 
-  // returns een array met alleen de items waar done = false
+  // returns een array van ToDo's met alleen de items waar done = false
   getOnlyDones(): ToDo[]{
     return this.todos.filter(f => f.done === true);
   }
 
-
-
-  // Working on 
-  getToDoById(id: number): ToDo[]{
-    let getid: number;
-    getid = id;
-    return this.todos.filter(i => i.id === getid)
-    
+  // Get Todo object met ID nummmer - dit is een sub-methode van updateTodoById
+  getTodoById(id: number): ToDo{
+    return this.todos
+    .filter(todo => todo.id === id)
+    .pop();
   }
   
-  // Working on
-  updateToDoById(id: number){
-  this.getToDoById(id)
+  // Update the Todo object => done to "true" by ID - this is a sub-methode of flipDoneSwitch
+  updateTodoById(id: number, values: Object = {}): ToDo{
+    let todo = this.getTodoById(id);
+    if(!todo){
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
   }
-
-  // Working on
-  flipDoneSwitch(todo){
-  this.updateToDoById(todo.id);
-
+  // Checkbox checked roept deze methode op om done attribuut op "false" te zetten.
+  flipDoneSwitch(todo: ToDo){
+    let justDoneTodo = this.updateTodoById(todo.id,{
+      done: !todo.done
+    });
+    return justDoneTodo;
   }
-  
-  
-
 }
+  
+  
+
+
